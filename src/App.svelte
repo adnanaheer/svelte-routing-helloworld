518266eafd86b0c0f73c8d1a23cc162d891677bd
Adnan Aheer <adnanaheer20@gmail.com>
2022-03-23 12:43:46 +0500

    implemented routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,32 @@
 <script>
 	export let name;
+	import { Router, Route, Link } from "svelte-routing";
+	import Home from "./pages/Home.svelte";
+	import About from "./pages/About.svelte";
+	export let url = ""; //This property is necessary declare to avoid ignore the Router
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Router url="{url}">
+		<nav>
+		   <Link to="/">Home</Link>
+		   <Link to="about">About</Link>
+		 </nav>
+		 <div>
+		   <Route path="about" component="{About}" /> 
+		   <!--for now the router just support case sensitive,
+			   one workaround colud be add two time the route
+			   Example.
+			  <Route path="About" component="{About}" /> 
+		   -->
+		   <Route path="/"><Home /></Route>
+		 </div>
+	   </Router>
+	<!-- <h1>Hello {name}!</h1>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p> -->
 </main>
 
 <style>
